691cc3acdd7ba0994835b3a93a609419f51326b0
Eduardo Garbosa <louiskyumi@gmail.com>
2025-09-26 11:49:05 -0300

    Create tentativas de senhas.cpp

---
+++ tentativas de senhas.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    
+    for(int i= 0;i <=9; i++){
+        for(int x= 0;x <=9; x++){
+            for(int y= 0;y <=9; y++){
+                for(int z= 0;z <=9; z++){
+            
+            printf("tentativas: %d %d %d %d \n",i,x,y,z);
+                }
+             }
+        }
+    }printf("neymar da silva da silvinha");
+
+}
